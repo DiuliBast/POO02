7cdfbccc4a5585afdb59b9928369412b16f9bd5d
diuli <diulikuhn.aluno@unipampa.edu.br>
2021-03-20 17:52:52 -0300

    Terceiro Commit

--- TrabalhoPOO2/src/BancodeMidias.java
+++ TrabalhoPOO2/src/BancodeMidias.java
@@ -3,5 +3,58 @@ import java.util.Scanner;
 public class BancodeMidias {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        Filme f;
+        Musica m;
+        Foto foto;
+        int op;
+
+
+        System.out.println("-----------------");
+        System.out.println("| Seja bem-vindo |");
+        System.out.println("-----------------");
+        System.out.println(" ");
+
+
+        do {
+            System.out.println("1. Adicionar um Filme");
+            System.out.println("2. Exibir um Filme");
+            System.out.println("0. sair ");
+            op = entrada.nextInt();
+            switch (op) {
+                case 1:
+                    f = new Filme();
+                    System.out.println("Adicione o produto");
+                    System.out.println("Escreva o título do filme: ");
+                    f.setTitulo(entrada.next());
+                    System.out.println("Escreva a descricao: ");
+                    f.setDescricao(entrada.next());
+                    System.out.println("Escreva o genero: ");
+                    f.setGenero(entrada.nextLine());
+                    System.out.println("Escreva o idioma: ");
+                    f.setIdioma(entrada.nextLine());
+                    System.out.println("Escreva o nome do diretor: ");
+                    f.setDiretor(entrada.nextLine());
+                    System.out.println("Escreva o Autor: ");
+                    f.setAtoresPrincipais(entrada.nextLine());
+                    System.out.println("Digite a duração do filme: ");
+                    f.setDuracao(entrada.nextDouble());
+                    System.out.println("Digite o ano: ");
+                    f.setAno(entrada.nextInt());
+                    f.criaArquivo(f);
+
+                    break;
+
+                case 2:
+                    System.out.println("Exibir Filmes");
+                    System.out.println(Filme.tudoString());
+                    break;
+
+                case 0:
+                    System.out.println("Você escolheu sair");
+                    break;
+            }
+
+
+        } while (op != 0);
     }
 }
--- TrabalhoPOO2/src/Filme.java
+++ TrabalhoPOO2/src/Filme.java
@@ -1,21 +1,26 @@
-public class Filme {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+public class Filme implements IFilme {
 
     private String titulo;
     private String descricao;
     private String genero;
     private String idioma;
     private String diretor;
-    private String atoresprincipais;
+    private String atoresPrincipais;
     private double duracao;
     private int ano;
 
-    public Filme (String titulo, String descricao, String genero, String idioma, String diretor, String atoresprincipais, double duracao, int ano){
+    public Filme (String titulo, String descricao, String genero, String idioma, String diretor, String atoresPrincipais, double duracao, int ano){
         this.titulo= titulo;
         this.descricao= descricao;
         this.genero= genero;
         this.idioma=idioma;
         this.diretor=diretor;
-        this.atoresprincipais=atoresprincipais;
+        this.atoresPrincipais=atoresPrincipais;
         this.duracao=duracao;
         this.ano=ano;
     }
@@ -52,10 +57,121 @@ public Filme (){
     public void setDiretor(String diretor){
         this.diretor=diretor;
     }
-    public String getAtoresprincipais(){
-        return atoresprincipais;
+    public String getAtoresPrincipais(){
+        return atoresPrincipais;
     }
-    public void setAtoresprincipais(String atoresprincipais){
-        this.atoresprincipais= atoresprincipais;
+    public void setAtoresPrincipais(String atoresPrincipais){
+        this.atoresPrincipais= atoresPrincipais;
+    }
+
+    public double getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(double duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void criaArquivo(Filme f){
+        Filme filme1;
+        String linha;
+        File arqFilme = new File ("/tmp/ListaDeFilmes.txt");
+
+        try {
+            if (!arqFilme.exists()) {
+                arqFilme.createNewFile();
+            } } catch (IOException e) {
+        }
+
+        try {
+            FileReader frFilme = new FileReader(arqFilme);
+            BufferedReader brFilme = new BufferedReader(frFilme);
+
+
+            while (brFilme.ready()) {
+
+                linha = brFilme.readLine();
+                String dados [] = linha.split(";");
+
+
+                titulo = dados [0];
+                descricao = dados[1];
+                genero = dados[2];
+                idioma = dados[3];
+                diretor = dados[4];
+                atoresPrincipais = dados[5];
+                duracao = Double.parseDouble(dados[6]);
+                ano = Integer.parseInt(dados[7]);
+
+                filme1 = new Filme(titulo, descricao, genero, idioma,diretor, atoresPrincipais, duracao,ano);
+                filmes.add(filme1);
+
+            }
+
+            brFilme.close();
+            frFilme.close();
+
+
+        } catch (IOException e) {
+
+        }
+
+
+    }
+
+    public static String tudoString(){
+        String tudo = "";
+        for(Filme f: filmes){
+            tudo = tudo + f.toString() + "\n";
+
+        }
+        return tudo;
+    }
+
+    @Override
+    public String toString() {
+        return "Título = " + titulo +
+                ", Descrição = '" + descricao + '\'' +
+                ", Gênero = '" + genero + '\'' +
+                ", Idioma = " + idioma + '\'' +
+                ", Atores Principais = " + atoresPrincipais + '\'' +
+                ", Duração = " + duracao + '\'' +
+                ", Ano = " + ano + '\'' +
+                ", Diretor = "  + diretor;
+    }
+
+    @Override
+    public boolean addFilme(Filme f) {
+        if(f == null){
+            return false;
+        }
+        if (filmes.add(f))
+
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public Filme getFilme(String titulo) {
+        return null;
+    }
+
+    @Override
+    public boolean editarFilme(String titulo, String genero) {
+        return false;
+    }
+
+    @Override
+    public boolean removeFime(String titulo) {
+        return false;
     }
 }
