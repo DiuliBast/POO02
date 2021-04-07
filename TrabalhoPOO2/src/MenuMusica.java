a90952b6d80d1073990c222122974b97ea243c35
Marcus Andrade <marcusbarbosa.aluno@unipampa.edu.br>
2021-04-07 17:42:51 -0300

    Commit

--- TrabalhoPOO2/src/MenuMusica.java
+++ TrabalhoPOO2/src/MenuMusica.java
@@ -4,7 +4,6 @@ public class MenuMusica {
     private JButton addMusicaButton;
     private JButton removeMusicaButton;
     private JButton exibirMusicaButton;
-    private JButton exibirButton;
     private JButton consultarButton;
     private JButton editarButton;
     private JButton removerButton;
