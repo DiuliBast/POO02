85df0451cc283bf7253539a0650ff99d19491797
diuli <diulikuhn.aluno@unipampa.edu.br>
2021-04-07 17:56:15 -0300

    CommitDiuliBastKuhnAluna

---
+++ TrabalhoPOO2/src/ConsultarFoto.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class ConsultarFoto {
+    private JLabel firstMainText;
+    private JTextField textField1;
+    private JButton OKButton;
+}
---
+++ TrabalhoPOO2/src/RemoverFoto.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class RemoverFoto {
+    private JLabel firstMainText;
+    private JTextField textField1;
+    private JButton OKButton;
+}
